ca0916c0b8d18b1b42bc766b865fbe93c920f5c7
Dominik Moritz <domoritz@gmail.com>
2022-10-02 22:52:51 -0400

    Support updates

--- src/bar-chart.ts
+++ src/bar-chart.ts
@@ -1,10 +1,6 @@
 import * as d3 from "d3";
 
-export function barChart(X: Int32Array, Y: string[]) {
-  // Here we use an array of indexes as data for D3 so we can process columnar data more easily
-  // but you could also directly pass an array of objects to D3.
-  const I = d3.range(X.length);
-
+export function barChart() {
   const margin = { top: 30, right: 0, bottom: 30, left: 50 };
   const width = document.body.clientWidth;
   const height = 300;
@@ -13,11 +9,8 @@ export function barChart(X: Int32Array, Y: string[]) {
   const yRange = [margin.top, height - margin.bottom];
 
   // Construct scales and axes.
-  const xScale = d3
-    .scaleLinear()
-    .domain([0, Math.max(...X)])
-    .range(xRange);
-  const yScale = d3.scaleBand().domain(Y).range(yRange).padding(0.1);
+  const xScale = d3.scaleLinear().range(xRange);
+  const yScale = d3.scaleBand().range(yRange).padding(0.1);
 
   const xAxis = d3.axisTop(xScale).ticks(width / 80);
   const yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
@@ -33,63 +26,91 @@ export function barChart(X: Int32Array, Y: string[]) {
   // Add the x axis
   svg
     .append("g")
-    .attr("transform", `translate(0,${margin.top})`)
-    .call(xAxis)
-    // add gridlines
-    .call((g) =>
-      g
-        .selectAll(".tick line")
-        .clone()
-        .attr("y2", height - margin.top - margin.bottom)
-        .attr("stroke-opacity", 0.1)
-    )
-    .call((g) =>
-      g
-        .append("text")
-        .attr("x", width - margin.right)
-        .attr("y", -22)
-        .attr("fill", "black")
-        .attr("text-anchor", "end")
-        .text("Count →")
-    );
+    .attr("class", "xaxis")
+    .attr("transform", `translate(0,${margin.top})`);
 
-  // Add the bars
+  // Add the y axis
   svg
     .append("g")
-    .attr("fill", "steelblue")
-    .selectAll("rect")
-    .data(I)
-    .join("rect")
-    .attr("x", xScale(0))
-    .attr("y", (i) => yScale(Y[i])!)
-    .attr("width", (i) => xScale(X[i]) - xScale(0))
-    .attr("height", yScale.bandwidth());
+    .attr("class", "yaxis")
+    .attr("transform", `translate(${margin.left},0)`);
+
+  // Add the bars
+  const bars = svg.append("g").attr("fill", "steelblue");
 
   // Add text
-  svg
+  const texts = svg
     .append("g")
     .attr("fill", "white")
     .attr("text-anchor", "end")
     .attr("font-family", "sans-serif")
-    .attr("font-size", 10)
-    .selectAll("text")
-    .data(I)
-    .join("text")
-    .attr("x", (i) => xScale(X[i]))
-    .attr("y", (i) => yScale(Y[i])! + yScale.bandwidth() / 2)
-    .attr("dy", "0.35em")
-    .attr("dx", -4)
-    .text((i) => xScale.tickFormat(100, "d")(X[i])!)
-    .call((text) =>
-      text
-        .filter((i) => xScale(X[i]) - xScale(0) < 20) // short bars
-        .attr("dx", +4)
-        .attr("fill", "black")
-        .attr("text-anchor", "start")
-    );
+    .attr("font-size", 10);
 
-  // Add the y axis
-  svg.append("g").attr("transform", `translate(${margin.left},0)`).call(yAxis);
+  function update(X: Int32Array, Y: string[]) {
+    // Here we use an array of indexes as data for D3 so we can process columnar data more easily
+    // but you could also directly pass an array of objects to D3.
+    const I = d3.range(X.length);
 
-  return svg.node()!;
+    xScale.domain([0, Math.max(...X)]);
+    yScale.domain(Y);
+
+    // Update the marks
+    bars
+      .selectAll("rect")
+      .data(I)
+      .join("rect")
+      .attr("x", xScale(0))
+      .attr("y", (i) => yScale(Y[i])!)
+      .attr("width", (i) => xScale(X[i]) - xScale(0))
+      .attr("height", yScale.bandwidth());
+
+    texts
+      .selectAll("text")
+      .data(I)
+      .join("text")
+      .attr("x", (i) => xScale(X[i]))
+      .attr("y", (i) => yScale(Y[i])! + yScale.bandwidth() / 2)
+      .attr("dy", "0.35em")
+      .attr("dx", -4)
+      .text((i) => xScale.tickFormat(100, "d")(X[i])!)
+      .call((text) =>
+        text
+          .filter((i) => xScale(X[i]) - xScale(0) < 20) // short bars
+          .attr("dx", +4)
+          .attr("fill", "black")
+          .attr("text-anchor", "start")
+      );
+
+    // Clear the axis so that when we add the grid, we don't get duplicate lines
+    svg.select(".xaxis").selectAll("*").remove();
+
+    // Update axes since we set new domains
+    svg
+      .select<SVGSVGElement>(".xaxis")
+      .call(xAxis)
+      // add gridlines
+      .call((g) =>
+        g
+          .selectAll(".tick line")
+          .clone()
+          .attr("y2", height - margin.top - margin.bottom)
+          .attr("stroke-opacity", 0.1)
+      )
+      .call((g) =>
+        g
+          .append("text")
+          .attr("x", width - margin.right)
+          .attr("y", -22)
+          .attr("fill", "black")
+          .attr("text-anchor", "end")
+          .text("Count →")
+      );
+
+    svg.select<SVGSVGElement>(".yaxis").call(yAxis);
+  }
+
+  return {
+    element: svg.node()!,
+    update,
+  };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import "./style.css";
+import * as d3 from "d3";
+
 import { barChart } from "./bar-chart";
 import { Int32, Table, Utf8 } from "apache-arrow";
 import { db } from "./duckdb";
 import parquet from "./weather.parquet?url";
+
+const app = document.querySelector("#app")!;
+
+// Create the chart. The specific code here makes some assumptions that may not hold for you.
+const chart = barChart();
+
+async function update(location: string) {
+  // Query DuckDB for the data we want to visualize.
+  const data: Table<{ weather: Utf8; cnt: Int32 }> = await conn.query(`
+  SELECT weather, count(*)::INT as cnt
+  FROM weather.parquet
+  WHERE location = '${location}'
+  GROUP BY weather
+  ORDER BY cnt DESC`);
+
+  // Get the X and Y columns for the chart. Instead of using Parquet, DuckDB, and Arrow, we could also load data from CSV or JSON directly.
+  const X = data.getChild("cnt")!.toArray();
+  const Y = data
+    .getChild("weather")!
+    .toJSON()
+    .map((d) => `${d}`);
+
+  chart.update(X, Y);
+}
 
 // Load a Parquet file and register it with DuckDB. We could request the data from a URL instead.
 const res = await fetch(parquet);
@@ -11,24 +37,27 @@ await db.registerFileBuffer(
   new Uint8Array(await res.arrayBuffer())
 );
 
-// Query DuckDB for the data we want to visualize.
+// Query DuckDB for the locations.
 const conn = await db.connect();
 
-const data: Table<{ weather: Utf8; cnt: Int32 }> = await conn.query(`
-SELECT "weather", count(*)::INT as cnt
-FROM weather.parquet
-GROUP BY "weather"
-ORDER BY cnt DESC`);
+const locations: Table<{ location: Utf8 }> = await conn.query(`
+SELECT DISTINCT location
+FROM weather.parquet`);
 
-// Get the X and Y columns for the chart. Instead of using Parquet, DuckDB, and Arrow, we could also load data from CSV or JSON directly.
-const X = data.getChild("cnt")!.toArray();
-const Y = data
-  .getChild("weather")!
-  .toJSON()
-  .map((d) => `${d}`);
+// Create a select element for the locations.
+const select = d3.select(app).append("select");
+for (const location of locations) {
+  select.append("option").text(location.location);
+}
 
-// Create the chart. The specific code here makes some assumptions that may not hold for you.
-const chart = barChart(X, Y);
+select.on("change", async () => {
+  const location = select.property("value");
+
+  update(location);
+});
+
+// Update the chart with the first location.
+update("Seattle");
 
 // Add the chart to the DOM.
-document.querySelector("#app")!.appendChild(chart);
+app.appendChild(chart.element);
